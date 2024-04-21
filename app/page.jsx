c4798bfb7ea176a2171469c8fd04a86f73528ad4
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-04-21 19:04:30 -0300

    configurações de estilo adicionadas

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,45 @@
+import Image from "next/image"
+
+import Dashboard from "@/sections/Dashboard"
+
+import SidebarItem from "@/components/SidebarItem"
+
+import Logo from '@/assets/logo.svg'
+
+import DashboardIcon from "@/assets/icon-home.svg"
+
 const Home = () => {
-  return (
-    <div>page</div>
-  )
+   return (
+      <>
+         <nav className="home__sidebar">
+
+            <Image
+               src={Logo}
+               alt="logo"
+               width={40}
+               height={40}
+               className="sidebar__logo"
+            />
+
+            <div className="sidebar__items-wrapper">
+
+               <div className="sidebar__items-wrapper__main-items">
+
+                  <SidebarItem 
+                     image={DashboardIcon}
+                     text="Dashboard"
+                     alt="dashboard icon"
+                  />
+
+               </div>
+
+            </div>
+
+         </nav>
+
+         <Dashboard />
+      </>
+   )
 }
 
 export default Home
