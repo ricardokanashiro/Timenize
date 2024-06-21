a79e4de0287d2a886d739cae42d019e628e4c617
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-06-21 14:24:37 -0300

    adicionando display: swap nas configurações de Fira Code

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,8 @@ import { ModalsContext } from "@/components/ModalsContext"
 
 const firaCode = Fira_Code({
    subsets: ['latin'],
-   weight: ['300', '400', '500', '600', '700']
+   weight: ['300', '400', '500', '600', '700'],
+   display: "swap",
 })
 
 const Home = () => {
